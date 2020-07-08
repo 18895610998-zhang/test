053d88468558d312d49cbd1f3c4f72fe2136d7f2
zhangjia <18895610998@163.com>
2020-07-08 18:48:30 +0800

    '初始化文件,创建了B组件'

---
+++ src/components/B.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<p>{{msg}}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name:"B",
+	data(){
+		return {
+			msg:"我是B组件"
+		}
+	}
+}
+</script>
+
+<style lang="stylus">
+
+</style>
